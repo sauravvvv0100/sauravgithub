b4a94cb2f9e14e9f26e9f2f7d6b28699123830a4
saurav <missssss17717.gmail.com>
2025-05-23 11:36:10 +0545

    pushing in github

--- saurav.c
+++ saurav.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
 #include<conio.h>
 int main()
 {
     int a,b;
         printf("enter a and b");
         scanf("%d%d",&a,&b);
-        
+        a=a+b;
+        printf("sum is %d\")
     return 0;
 }
